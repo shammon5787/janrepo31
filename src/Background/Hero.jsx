2c2b8f539091d0d86a9aa39b0b2e9d2f1de40b9f
shammon5787 <sadam.bsit@gmail.com>
2025-02-11 12:26:38 +0500

    a

--- src/Background/Hero.jsx
+++ src/Background/Hero.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 
-const Hero = ({ heroCount, palystatus, setheroCount, setPlayStatus }) => {
+const Hero = ({ heroCount, palystatus, setheroCount, setPlayStatus ,setData}) => {
     return (
         <div className='p-6'>
+           
             <div className='flex flex-row items-center bg-slate-950 w-[20vw] justify-between p-10 rounded-md opacity-60'>
                 <li onClick={()=>setheroCount(0)}  className={`list-none text-amber-300 cursor-pointer text-3xl bg-black rounded-full `}>⨀</li>
                 <li onClick={()=>setheroCount(1)}  className={`list-none text-amber-300 cursor-pointer text-3xl bg-black rounded-full`}>⨀</li>
