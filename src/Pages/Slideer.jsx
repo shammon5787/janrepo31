70e71cccb40dd1635418deb35a27b88c5d41ba7d
shammon5787 <sadam.bsit@gmail.com>
2025-02-14 22:37:21 +0500

    ff

--- src/Pages/Slideer.jsx
+++ src/Pages/Slideer.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Slider from '../Slide/Slider'
 
 const Slideer = () => {
   return (
-    <div className='mt-24'>
-        slideer
+    <div className='mt-24 p-5'>
+        <Slider/>
     </div>
   )
 }
---
+++ src/Slide/Slider.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import FoodData from '../assets/Data/FoodData.js'
+
+const Slider = () => {
+    return (
+        <div className='bg-slate-800 w-[90vw] m-auto rounded-lg opacity-70'> 
+            {
+                FoodData.map((item) => {
+                    return (
+                        <div className='flex flex-col items-center p-10 '>
+                            <img className='rounded-full w-[200px] h-[200px]' src={item.image} alt="" />
+                            <h1>{item.desc}</h1>
+                            <h1>{item.name}</h1>
+                        </div>
+                    )
+                })
+            }
+        </div>
+    )
+}
+
+export default Slider
